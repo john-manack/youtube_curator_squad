45d51a228a7c6ac65afb77b14d4c8a1738b8725e
Kurtis Garcia <kurtisgarcia14@gmail.com>
2021-02-11 16:55:19 -0500

    got list of videos to show up, link from contents page to video page working, app is functioning

--- routes/videos.js
+++ routes/videos.js
@@ -4,10 +4,12 @@ const express = require('express'),
     router = express.Router(),
     fetch = require('node-fetch');
 
-router.get('/', async (req, res) => {
+router.get('/q=:query', async (req, res) => {
+    const { query } = req.params; 
     const apiVideos = await fetch(
-        'https://www.googleapis.com/youtube/v3/search?part=snippet&order=viewCount&q=basketball&type=video&videoDefinition=high&key=AIzaSyBqP72gnX5iMsd_7Ij5CGND5OkLGuvY120'
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&order=viewCount&q=${query}&type=video&videoDefinition=high&key=AIzaSyCSM_aP_AujLtVzLcv3vXqgQBnCRFhUn6w&resultsPerPage=10&videoEmbeddable=true&maxResults=10`
     ).then((response) => response.json());
+    console.log(apiVideos.items[0].id.videoId);
 
     res.render('template', {
         locals: {
@@ -18,7 +20,28 @@ router.get('/', async (req, res) => {
         partials: {
             body: 'partials/content',
         }
-    })
+    });
+});
+
+router.post('/search', async (req, res) => {
+    const { search_input } = await req.body;
+    console.log("search input is ", search_input);
+    res.redirect(`/videos/q=${search_input}`);
+});
+
+router.get('/watch/:videoId', (req, res) => {
+    const { videoId } = req.params;
+    console.log(videoId);
+    res.render('template', {
+        locals: {
+            title: 'Watch video',
+            videoId,
+            is_logged_in: req.session.is_logged_in,
+        },
+        partials: {
+            body: 'partials/videoplayer',
+        }
+    });
 });
 
 module.exports = router;
